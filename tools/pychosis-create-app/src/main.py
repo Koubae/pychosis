6a51a1a06c1de405a02583c846b00d411f235cd6
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-01 14:44:39 +0100

    Requests project name and errors if already exists

--- tools/pychosis-create-app/src/main.py
+++ tools/pychosis-create-app/src/main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 from rich import pretty
 from rich.console import Console
+from rich.prompt import Prompt
 
 from tree_selector import TreeSelectorApp
 
@@ -25,6 +26,17 @@ def main() -> None:
         project_dir_selector = TreeSelectorApp(root_path=pychosis_root_directory)
         project_dir_selected = project_dir_selector.run()
         console.print(f"[green]Selected folder: [/] :open_file_folder: [bold]{project_dir_selected}[/]")
+
+    project_name = Prompt.ask("[bold cyan]Enter your project's name[/]")
+    console.print(f"New Project name: {project_name}", style="bold green")
+
+    project_path = Path(project_dir_selected) / project_name
+    if project_path.exists():
+        console.print(
+            f"[bold red]ERROR:[/bold red] Name '{project_name}' already exists in {project_dir_selected}",
+            style="white on red",
+        )
+        quit(1)
 
 
 if __name__ == "__main__":

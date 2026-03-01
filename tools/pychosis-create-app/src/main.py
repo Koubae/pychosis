de44fa83d7472794da7a1824b29d893beff00b38
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-01 11:56:08 +0100

    Iterate through pychosis project

--- tools/pychosis-create-app/src/main.py
+++ tools/pychosis-create-app/src/main.py
@@ -1,6 +1,34 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+from pathlib import Path
+
+IGNORE_DIRS: tuple[str, ...] = (".venv", ".git", "docs")
+PYCHOSIS_CONFIG_FILE = '.pychosis'
 
 def main():
-    print("Hello from pychosis-create-app!")
+    load_dotenv(find_dotenv(), override=False)
+
+    pychosis_root_directory_path = os.getenv("PYCHOSIS_ROOT_DIRECTORY")
+    if not pychosis_root_directory_path:
+        raise RuntimeError("PYCHOSIS_ROOT_DIRECTORY environment variable is not set")
+
+    pychosis_root_directory = Path(pychosis_root_directory_path)
+    pychosis_project_directories = []
+    for path in pychosis_root_directory.iterdir():
+        if not path.is_dir():
+            continue
+        if path.name in IGNORE_DIRS:
+            continue
+        if (path / "pyproject.toml").exists():
+            continue
+
+        pychosis_config_file = path / PYCHOSIS_CONFIG_FILE
+        is_pychosis_project_dir = pychosis_config_file.exists()
+        if not is_pychosis_project_dir:
+            continue
+
+        print(f"Adding {path} to directory list")
+        pychosis_project_directories.append(path)
 
 
 if __name__ == "__main__":

1163f804c25db3e860bcfc5340b3fcaf783abd2c
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-02 10:44:02 +0100

    Created Fastapi application

--- experiments/apps/event_ingestor/src/main.py
+++ experiments/apps/event_ingestor/src/main.py
@@ -1,5 +1,19 @@
+import logging
+import os
+
+import uvicorn
+
+from src.app.asgi import create_app
+
+
+app = create_app()
+
+
 def main():
-    print("Hello from event-ingestor!!!")
+    port = int(os.getenv("PORT", 8000))
+    host = os.getenv("HOST", "0.0.0.0")
+    log_level = logging.getLevelName(os.getenv("LOG_LEVEL", "INFO").upper())
+    uvicorn.run(app, host=host, port=port, log_level=log_level)
 
 
 if __name__ == "__main__":

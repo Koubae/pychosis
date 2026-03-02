c26f80a0d34ba2198de66df489b467a82e926841
koubae <58447627+Koubae@users.noreply.github.com>
2026-03-02 10:47:29 +0100

    setup logger

--- experiments/apps/event_ingestor/src/app/asgi.py
+++ experiments/apps/event_ingestor/src/app/asgi.py
@@ -1,6 +1,19 @@
+import logging
+import os
+
 from fastapi import FastAPI
 
 
 def create_app() -> FastAPI:
+    setup()
     app = FastAPI()
     return app
+
+
+def setup() -> None:
+    log_level = logging.getLevelName(os.getenv("LOG_LEVEL", "INFO").upper())
+    logging.basicConfig(level=log_level)
+    logging.getLogger("uvicorn").setLevel(log_level)
+    logging.getLogger("uvicorn.access").setLevel(log_level)
+    logging.getLogger("uvicorn.error").setLevel(log_level)
+    logging.getLogger("uvicorn.access").setLevel(log_level)
